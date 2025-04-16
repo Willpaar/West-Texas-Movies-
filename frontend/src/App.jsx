0e3f9c0d4e690b20aab115295838bed2e0e04696
unknown <williampaar@gmail.com>
2025-04-15 21:31:25 -0500

    finished homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return(
   <div>
-
-
     <BrowserRouter>
-
         <Routes>
 
           <Route path='/' element={<Homepage/>}/>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
+import HomeBody from '../components/HomeBody.jsx'
 const Homepage = () => {
   return (
     <div>
         <Header/>
+        <HomeBody/>
     </div>
   )
 }
